423cb3a45e90513e78e02ebf53b7ebaef227474e
Jonathan Barron <jonathan.barron@smoothwall.net>
2015-11-16 09:02:18 +0000

    Gerrit: Add step to reindex before starting gerrit
    This allows Gerrit's web ui to index changes from the database and
    fixes the issue where changes were being copied into the database but
    were not showing up on the UI.

--- docker-sw-gerrit/gerrit-start.sh
+++ docker-sw-gerrit/gerrit-start.sh
@@ -7,5 +7,7 @@ psql -U gerrit2 -d reviewdb -h $DATABASE_HOSTNAME < /jenkins_user.sql
 
 java -jar /var/gerrit/review_site/bin/gerrit.war init -d /var/gerrit/review_site
 
+gosu gerrit2 java -jar /var/gerrit/gerrit.war reindex -d $GERRIT_SITE
+
 echo "Starting Gerrit..."
 exec gosu ${GERRIT_USER} $GERRIT_SITE/bin/gerrit.sh daemon
