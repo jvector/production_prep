3c65de05e2347f9cddb8f4bebd2b7b74effe9458
Jonathan Barron <jonathan.barron@smoothwall.net>
2015-12-01 16:22:54 +0000

    Reinstate Google OAuth for gerrit-container
    Turned OAuth back on, ready for public use.

--- docker-sw-gerrit/config.sh
+++ docker-sw-gerrit/config.sh
@@ -11,20 +11,24 @@ function set_secure_config {
   gosu ${GERRIT_USER} git config -f "${GERRIT_SITE}/etc/secure.config" $@
 }
 
-# Smoothwall specific settings
-# set_gerrit_config auth.trustedOpenID 'https://www.google.com/accounts/o8/id?id='
-# set_gerrit_config core.streamFileThreshold '100 m'
+# Possibly not needed
 # set_gerrit_config gitweb.url 'http://$HOST/'
+
+# Smoothwall specific settings
+set_gerrit_config auth.trustedOpenID 'https://www.google.com/accounts/o8/id?id='
+set_gerrit_config core.streamFileThreshold '100 m'
 set_gerrit_config gerrit.basePath '/usr/src/gerrit'
 # 
-# gosu ${GERRIT_USER} curl -L https://github.com/davido/gerrit-oauth-provider/releases/download/v0.3/gerrit-oauth-provider.jar -o ${GERRIT_SITE}/plugins/gerrit-oauth-provider.jar
+gosu ${GERRIT_USER} curl -L https://github.com/davido/gerrit-oauth-provider/releases/download/v0.3/gerrit-oauth-provider.jar -o ${GERRIT_SITE}/plugins/gerrit-oauth-provider.jar
 # 
-# set_gerrit_config auth.type OAUTH
-set_gerrit_config auth.type DEVELOPMENT_BECOME_ANY_ACCOUNT
-# set_gerrit_config auth.allowGoogleAccountUpgrade true
-# set_gerrit_config plugin.gerrit-oauth-provider-google-oauth.client-id 263251712050-3liqjq40ji336id337c63lo1evmf0d0g.apps.googleusercontent.com
-# set_gerrit_config plugin.gerrit-oauth-provider-google-oauth.link-to-existing-open-id-accounts true
-# set_secure_config plugin.gerrit-oauth-provider-google-oauth.client-secret _O3-5P73V3L9gQfyq_AeEKzo
+# For debug use ..
+# set_gerrit_config auth.type DEVELOPMENT_BECOME_ANY_ACCOUNT
+
+set_gerrit_config auth.type OAUTH
+set_gerrit_config auth.allowGoogleAccountUpgrade true
+set_gerrit_config plugin.gerrit-oauth-provider-google-oauth.client-id 263251712050-3liqjq40ji336id337c63lo1evmf0d0g.apps.googleusercontent.com
+set_gerrit_config plugin.gerrit-oauth-provider-google-oauth.link-to-existing-open-id-accounts true
+set_secure_config plugin.gerrit-oauth-provider-google-oauth.client-secret _O3-5P73V3L9gQfyq_AeEKzo
 
 # Sym link the /usr/src/buildsystem/gerrithooks to /var/gerrit/review_site/hooks. Where they're expected
 mkdir -p /var/gerrit/review_site/hooks 
