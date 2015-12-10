ec0f6e5c2c471d1e69f799b60a5b2c558415fea7
Jonathan Barron <jonathan.barron@smoothwall.net>
2015-12-10 17:09:11 +0000

    Changes ready for files now being on Buildfs
    Defaults has been changed to reflect the new folder structure:
    
    On buildfs we've made /srv/container_data/* which have been nfs mounted
    on buildwhale1 to /usr/src/buildfs-mounts/*
    
    We have a script prepared and ready to copy the contents of /usr/src/
    container-mounts onto buildfs.
    
    Existing on buildfs already were /srv/build (/mnt/build) and /home/build.
    
    We use the same /srv/build as metal buildsystem but /home/build we have
    a container replica because ssh config's and stuff need to be different

--- defaults.sh
+++ defaults.sh
@@ -9,22 +9,23 @@ DEV=${DEV:-0}
 
 # If your home dir is on sotonfs, or you want to use a dir other than $HOME
 # put an alternative in BUILD_HOME
-BUILD_HOME="${BUILD_HOME:-$HOME}/container-mounts"
+BUILD_HOME="${BUILD_HOME:-$HOME}/buildfs-mounts"
 DB_DUMPS=${BUILD_HOME:-$HOME}/db_dumps
 
-APTLY_DEBIANIZER_DATA=${BUILD_HOME:-$HOME}/aptly-debianizer-test/
-APTLY_DEBIANIZER_SERVE_DATA=${BUILD_HOME:-$HOME}/aptly-debianizer-serve-test/
-APTLY_S3_DATA=${BUILD_HOME:-$HOME}/aptly-s3-test/
-BUILDSYSTEM_DATA=${BUILD_HOME:-$HOME}/buildsystem-test/
-DEVMETADATA_DATA=${BUILD_HOME:-$HOME}/dev-metadata-test/
-GERRIT_GIT_DATA=${BUILD_HOME:-$HOME}/gerrit-git-data/
-HOME_BUILD_DATA=${BUILD_HOME:-$HOME}/home-build-test/
-VAR_GERRIT_SSH_DATA=${BUILD_HOME:-$HOME}/var-gerrit-ssh-test/
-GERRIT_DATA=${BUILD_HOME:-$HOME}/gerrit-test/
-JENKINS_DATA=${BUILD_HOME:-$HOME}/jenkins-test/
 MNTBUILD_DATA=${BUILD_HOME:-$HOME}/mnt-build-test/
-PG_BUGZILLA_DATA=${BUILD_HOME:-$HOME}/pg-bugzilla-test/
-PG_GERRIT_DATA=${BUILD_HOME:-$HOME}/postgres-test/
-REPO_DATA=${BUILD_HOME:-$HOME}/repo-test/
-SRV_CHROOT_DATA=${BUILD_HOME:-$HOME}/srv-chroot-data/
-ETC_SCHROOT_CHROOTD=${BUILD_HOME:-$HOME}/etc-schroot-chrootd-data/
+
+APTLY_DEBIANIZER_DATA=${BUILD_HOME:-$HOME}/container_data/aptly-debianizer
+APTLY_DEBIANIZER_SERVE_DATA=${BUILD_HOME:-$HOME}/container_data/aptly-debianizer-serve
+APTLY_S3_DATA=${BUILD_HOME:-$HOME}container_data/aptly-s3
+BUILDSYSTEM_DATA=${BUILD_HOME:-$HOME}container_data/buildsystem
+DEVMETADATA_DATA=${BUILD_HOME:-$HOME}container_data/dev-metadata
+HOME_BUILD_DATA=${BUILD_HOME:-$HOME}container_data/home
+GERRIT_GIT_DATA=${BUILD_HOME:-$HOME}container_data/gerrit-gits
+VAR_GERRIT_SSH_DATA=${BUILD_HOME:-$HOME}container_data/var-gerrit-ssh
+GERRIT_DATA=${BUILD_HOME:-$HOME}container_data/gerrit-home
+JENKINS_DATA=${BUILD_HOME:-$HOME}container_data/jenkins-home
+PG_BUGZILLA_DATA=${BUILD_HOME:-$HOME}container_data/pg-bugzilla-db
+PG_GERRIT_DATA=${BUILD_HOME:-$HOME}container_data/pg-gerrit-db
+REPO_DATA=${BUILD_HOME:-$HOME}container_data/repository
+SRV_CHROOT_DATA=${BUILD_HOME:-$HOME}container_data/srv-chroot
+ETC_SCHROOT_CHROOTD=${BUILD_HOME:-$HOME}container_data/etc-schroot-chrootd
