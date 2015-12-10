a6279e7c925439f96910689819b2508a69d50a33
Jonathan Barron <jonathan.barron@smoothwall.net>
2015-12-10 09:43:57 +0000

    Change mergedrepo to point back to aptly-debianizer

--- container_functions.sh
+++ container_functions.sh
@@ -528,7 +528,7 @@ function rm_internal_repo {
 function build_merged_repo {
     docker build \
         -t ${MERGED_REPO_IMAGE} \
-        --build-arg SERVER_ROOT=/mnt/aptly-debianizer-serve/live \
+        --build-arg SERVER_ROOT=/mnt/aptly-debianizer \
         docker-sw-lighttpd || \
     fail "Building image ${MERGED_REPO_IMAGE} failed"
 }
@@ -537,7 +537,7 @@ function run_merged_repo {
     echo "Starting Merged Repository.."
     docker run \
         --name ${MERGED_REPO_NAME} \
-        -v ${APTLY_DEBIANIZER_SERVE_DATA}:/mnt/aptly-debianizer-serve \
+        -v ${APTLY_DEBIANIZER_DATA}:/mnt/aptly-debianizer \
         --net=${DOCKER_NETWORK} \
         -d ${MERGED_REPO_IMAGE}
     echo "Merged repository container ${MERGED_REPO_NAME} running."
