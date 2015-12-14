1808a04b3b7f9b38377aded1a29dfa5760eb4800
Jonathan Barron <jonathan.barron@smoothwall.net>
2015-12-14 12:16:56 +0000

    Change the defaults file back to using on disk volumes
    Revoking NFS mounting for performance & for breaking locking

--- defaults.sh
+++ defaults.sh
@@ -9,23 +9,25 @@ DEV=${DEV:-0}
 
 # If your home dir is on sotonfs, or you want to use a dir other than $HOME
 # put an alternative in BUILD_HOME
-BUILD_HOME="${BUILD_HOME:-$HOME}/buildfs-mounts"
+BUILD_HOME_CONT="${BUILD_HOME:-$HOME}/container-mounts"
 DB_DUMPS=${BUILD_HOME:-$HOME}/db_dumps
 
-MNTBUILD_DATA=${BUILD_HOME:-$HOME}/mnt-build/
+# Mnt build is NFS mounted
+MNTBUILD_DATA=${BUILD_HOME:-$HOME}/buildfs-mounts/mnt-build/
 
-APTLY_DEBIANIZER_DATA=${BUILD_HOME:-$HOME}/container_data/aptly-debianizer
-APTLY_DEBIANIZER_SERVE_DATA=${BUILD_HOME:-$HOME}/container_data/aptly-debianizer-serve
-APTLY_S3_DATA=${BUILD_HOME:-$HOME}/container_data/aptly-s3
-BUILDSYSTEM_DATA=${BUILD_HOME:-$HOME}/container_data/buildsystem
-DEVMETADATA_DATA=${BUILD_HOME:-$HOME}/container_data/dev-metadata
-HOME_BUILD_DATA=${BUILD_HOME:-$HOME}/container_data/home
-GERRIT_GIT_DATA=${BUILD_HOME:-$HOME}/container_data/gerrit-gits
-VAR_GERRIT_SSH_DATA=${BUILD_HOME:-$HOME}/container_data/var-gerrit-ssh
-GERRIT_DATA=${BUILD_HOME:-$HOME}/container_data/gerrit-home
-JENKINS_DATA=${BUILD_HOME:-$HOME}/container_data/jenkins-home
-PG_BUGZILLA_DATA=${BUILD_HOME:-$HOME}/container_data/pg-bugzilla-db
-PG_GERRIT_DATA=${BUILD_HOME:-$HOME}/container_data/pg-gerrit-db
-REPO_DATA=${BUILD_HOME:-$HOME}/container_data/repository
-SRV_CHROOT_DATA=${BUILD_HOME:-$HOME}/container_data/srv-chroot
-ETC_SCHROOT_CHROOTD=${BUILD_HOME:-$HOME}/container_data/etc-schroot-chrootd
+# Rest are on local disk
+APTLY_DEBIANIZER_DATA=${BUILD_HOME_CONT}/aptly-debianizer-test
+APTLY_DEBIANIZER_SERVE_DATA=${BUILD_HOME_CONT}/aptly-debianizer-serve-test
+APTLY_S3_DATA=${BUILD_HOME_CONT}/aptly-s3-test
+BUILDSYSTEM_DATA=${BUILD_HOME_CONT}/buildsystem-test
+DEVMETADATA_DATA=${BUILD_HOME_CONT}/dev-metadata-test
+HOME_BUILD_DATA=${BUILD_HOME_CONT}/home-build-test
+GERRIT_GIT_DATA=${BUILD_HOME_CONT}/gerrit-git-data
+VAR_GERRIT_SSH_DATA=${BUILD_HOME_CONT}/var-gerrit-ssh-test
+GERRIT_DATA=${BUILD_HOME_CONT}/gerrit-test
+JENKINS_DATA=${BUILD_HOME_CONT}/jenkins-test
+PG_BUGZILLA_DATA=${BUILD_HOME_CONT}/pg-bugzilla-test
+PG_GERRIT_DATA=${BUILD_HOME_CONT}/postgres-test
+REPO_DATA=${BUILD_HOME_CONT}/repo-test
+SRV_CHROOT_DATA=${BUILD_HOME_CONT}/srv-chroot-data
+ETC_SCHROOT_CHROOTD=${BUILD_HOME_CONT}/etc-schroot-chrootd-data
